49d24e08908c37af31a696d84b2514da01cb83b3
nggialam <gialam130704@gmail.com>
2026-05-30 14:32:32 +0700

    add app

---
+++ appquanlisach/appquanlisachTests/appquanlisachTests.swift
@@ -0,0 +1,19 @@
+//
+//  appquanlisachTests.swift
+//  appquanlisachTests
+//
+//  Created by linda on 30/5/26.
+//
+
+import Testing
+@testable import appquanlisach
+
+struct appquanlisachTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
